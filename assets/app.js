78b41e560c1e25243d4bee0a67936a74bfccbc84
eunjiruss <31670138+eunjiruss@users.noreply.github.com>
2017-11-11 01:48:23 -0800

    More stuff working.

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 var scifiList = ["Star Wars", "Blade Runner", "Alien", "2001: A Space Odyssey", "The Matrix", "Avatar", "District 9", "Interstellar", "The Terminator", "Inception"];
 
 
+
+
 function addButtonGiphyStuff(buttonName, i)
 {
 	$("#" + buttonName + i).on("click", function() {
@@ -8,7 +10,6 @@ function addButtonGiphyStuff(buttonName, i)
 		for (var j =0; j < 10;j++){
 		  // havea. string like "button0", want to get the number.
 		  var movieName = scifiList[i];
-
 	      // Storing our giphy API URL for a random cat image
 	      var queryURL = "https://api.giphy.com/v1/gifs/random";
 
@@ -20,24 +21,42 @@ function addButtonGiphyStuff(buttonName, i)
 	        	api_key : "dc6zaTOxFJmzC",
 	        	tag : movieName
 	        }
-	      })
+	      }).done(function(response) {
+	      	console.log(response)
+	        var stillUrl = response.data.fixed_height_small_still_url;
+	        var animateUrl = response.data.image_original_url;
 
-	      .done(function(response) {
-
-	        var imageUrl = response.data.image_original_url;
 
 	        var moviegif = $("<img>");
 
-	        moviegif.attr("src", imageUrl);
-	        moviegif.attr("alt", "movies");
-
+	       	moviegif.attr("alt", "movies");
+	        moviegif.attr("data-state", "still");
+	        moviegif.attr("data-animate", animateUrl);
+	        moviegif.attr("data-still", stillUrl);
+	        moviegif.attr("src", stillUrl);
+	        moviegif.addClass("tester");
 	        $("#gif").prepend(moviegif);
-	      
 
-	        
+	        $(".tester").off("click").on("click", function() {
+		      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+		      var state = $(this).attr("data-state");
+		      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+		      // Then, set the image's data-state to animate
+		      // Else set src to the data-still value
+		      if (state === "still") {
+		        $(this).attr("src", $(this).attr("data-animate"));
+		        $(this).attr("data-state", "animate");
+		      } else {
+		        $(this).attr("src", $(this).attr("data-still"));
+		        $(this).attr("data-state", "still");
+		      }
+
+			});      
 	      });
 	  	}
 	  });
+
+
 }
 
 function renderButtons() {
@@ -61,6 +80,7 @@ function renderButtons() {
 
 
 $(document).ready(function(){
+
 
       $("#addMovie").on("click", function(event) {
         event.preventDefault();
